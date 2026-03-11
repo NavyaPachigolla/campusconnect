5526e9398a6b8ff2bf1c083e34faa4baed442081
NavyaPachigolla <24b01a12b5@svecw.edu.in>
2026-03-11 21:50:03 +0530

    connected to railway

--- campusconnect/settings.py
+++ campusconnect/settings.py
@@ -78,17 +78,12 @@ WSGI_APPLICATION = 'campusconnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-if os.getenv("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
 
 
 # Password validation
